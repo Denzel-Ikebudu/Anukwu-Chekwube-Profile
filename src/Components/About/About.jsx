ded27db105b6a96518555afb983fe88836b28002
Ikebudu Denzel <ikebududenzel@gmail.com>
2025-08-03 20:40:20 +0100

    Updates

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './About.css'
 import chekwube1 from '../Assets/profPic.png'
+import { useState } from 'react'
 
 const About = () => {
+  const[showFullContent, setShowFullContent]= useState(false);
+  const toggleContent = ()=> {
+    setShowFullContent(!showFullContent)
+  }
+
   return (
     <div className= 'about' id='about-section'>
         <div className='about-picture'>
             <img src={chekwube1} alt="" />
         </div>
         <div className="about-text" >
-            <h2>ABOUT <span>ME</span></h2>
+            <h2>ABOUT <span>ME</span></h2><br />
             <p>
               Hi, I’m Anukwu Chekwube Immanuel—a passionate creative 
               professional with 3+ years of experience in graphic 
@@ -27,11 +33,11 @@ const About = () => {
               collaborative process that puts your vision first. 
               At IMMAGINATION, my mission is simple: “Bringing your thoughts 
               to life.”
-            </p>
-            <h2>ABOUT <span>IMMAGINATION</span></h2>
+            </p> <br />
+            <h2>ABOUT <span>IMMAGINATION</span></h2> <br />
             <p>
               <h3>Brand Name: IMMAGINATION</h3> <br />
-              <h3>Tagline: Bringing your thoughts to life</h3> <br /><br />
+              <h3>Tagline: Bringing your thoughts to life</h3> <br />
 
               At IMMAGINATION, creativity meets purpose. We are a dynamic design and print brand specializing 
               in Graphics Design, Brand Identity Creation, and High-Quality Printing solutions. Whether you're 
@@ -40,52 +46,58 @@ const About = () => {
 
               From eye-catching banners, handbills, flyers, and branded frames to custom polos, T-shirts, and 
               corporate merch, we craft every detail to make your message stand out. Our design philosophy is simple: 
-              Create with intention. Design with impact. Deliver with excellence.
-              <br /><br />
+              Create with intention. Design with impact. Deliver with excellence
+              <button onClick={toggleContent} className='see-more-button'>
+                {showFullContent? 'See less' : 'See more'}
+              </button> <br /><br />
+              {showFullContent && (
+                <div className='more-about-content'>
+                  We don’t just design graphics—we build identity systems that speak for you when you’re not in the room. 
+                  Whether you're launching a startup, hosting an event, or rebranding your business, 
+                  IMMAGINATION helps you look your best and communicate your value clearly: 
+                  For simplicity in communication is a complexity and  a sophistication in itself.
+                  <br /><br />
+                  <h3>Brand Essence</h3><br />
+                  IMMAGINATION is a forward-thinking creative brand that transforms bold ideas into impactful visual and 
+                  experiential realities. We champion innovation, empower individuality, and fuse artistry with strategy to shape 
+                  distinctive identities in a world that thrives on originality and impact driven.
+                  <br /><br />
 
-              We don’t just design graphics—we build identity systems that speak for you when you’re not in the room. 
-              Whether you're launching a startup, hosting an event, or rebranding your business, 
-              IMMAGINATION helps you look your best and communicate your value clearly: 
-              For simplicity in communication is a complexity and  a sophistication in itself.
-              <br /><br />
-              <h3>Brand Essence</h3><br />
-              IMMAGINATION is a forward-thinking creative brand that transforms bold ideas into impactful visual and 
-              experiential realities. We champion innovation, empower individuality, and fuse artistry with strategy to shape 
-              distinctive identities in a world that thrives on originality and impact driven.
-              <br /><br />
+                  <h3>Mission</h3><br />
 
-              <h3>Mission</h3><br />
+                  To ignite limitless creativity through powerful storytelling, design, and innovation—helping individuals and 
+                  brands express who they truly are with clarity, elegance, and immagination.
+                  <br /><br />
+                  
+                  <h3>Vision</h3><br />
 
-              To ignite limitless creativity through powerful storytelling, design, and innovation—helping individuals and 
-              brands express who they truly are with clarity, elegance, and immagination.
-              <br /><br />
-              
-              <h3>Vision</h3><br />
+                  To become a global catalyst for imaginative transformation, where design, technology, and identity converge to 
+                  inspire connection, emotion, change and impact.
+                  <br /><br />
 
-              To become a global catalyst for imaginative transformation, where design, technology, and identity converge to 
-              inspire connection, emotion, change and impact.
-              <br /><br />
+                  <h3>Core Values</h3><br />
+                  •	Creativity Without Limits – We believe imagination has no boundaries. <br />
+                  •	Authenticity – Every brand and story deserves to be told in its truest form. <br />
+                  •	Innovation – We evolve constantly, embracing boldness and technology. <br />
+                  •	Empowerment – We elevate our clients and audience through purposeful and clear visual expression. <br />
+                  •	Excellence – Every detail matters; we craft with intention and precision. <br />
+                  <br /><br />
 
-              <h3>Core Values</h3><br />
-              •	Creativity Without Limits – We believe imagination has no boundaries. <br />
-              •	Authenticity – Every brand and story deserves to be told in its truest form. <br />
-              •	Innovation – We evolve constantly, embracing boldness and technology. <br />
-              •	Empowerment – We elevate our clients and audience through purposeful and clear visual expression. <br />
-              •	Excellence – Every detail matters; we craft with intention and precision. <br />
-              <br /><br />
+                  <h3>Brand Personality</h3><br />
+                  •	Visionary <br />
+                  •	Sophisticated <br />
+                  •	Bold <br />
+                  •	Empowering <br />
+                  •	Inspirational <br /><br />  
 
-              <h3>Brand Personality</h3><br />
-              •	Visionary <br />
-              •	Sophisticated <br />
-              •	Bold <br />
-              •	Empowering <br />
-              •	Inspirational <br /><br />  
+                  <h3>Positioning Statement</h3> <br />
 
-              <h3>Positioning Statement</h3> <br />
+                  For creatives, visionaries, and bold brands seeking to express their true essence, IMMAGINATION is the 
+                  creative force that transforms ideas into unforgettable identities—delivering clarity, emotion, and impact 
+                  through artistic excellence and innovative thinking.
 
-              For creatives, visionaries, and bold brands seeking to express their true essence, IMMAGINATION is the 
-              creative force that transforms ideas into unforgettable identities—delivering clarity, emotion, and impact 
-              through artistic excellence and innovative thinking.
+                </div>
+              )}
             </p>
         </div>
       
