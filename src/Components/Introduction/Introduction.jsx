17d7a05ed57d8179a02e112214858be8c8563343
Ikebudu Denzel <ikebududenzel@gmail.com>
2025-08-03 21:06:40 +0100

    more updates

--- src/Components/Introduction/Introduction.jsx
+++ src/Components/Introduction/Introduction.jsx
@@ -7,28 +7,11 @@ const Introduction = () => {
         <span>INTRO</span>DUCTION
       </h2>
       <p>
-        Anukwu Chekwube Immanuel is a passionate and results-driven 
-        creative professional with over three years of hands-on experience
-        in graphics design, brand identity development, video editing, 
-        and UI/UX design. With a keen eye for aesthetics and a strategic 
-        mindset, he specializes in transforming abstract ideas into impactful 
-        visual experiences.
-
-        As a brand identity strategist, Immanuel helps individuals, 
-        startups, and businesses craft compelling visual languages 
-        that communicate authenticity, clarity, and purpose. His 
-        approach blends design thinking with deep customer empathy, 
-        ensuring every project not only looks good but also connects
-         meaningfully with its target audience.
-
-        Beyond his technical skills, Immanuel is known for his 
-        excellent customer service, collaborative spirit, and 
-        unwavering commitment to delivering value through creativity. 
-        Whether he's designing a logo, building a user interface, or 
-        shaping a brand’s story, he brings vision, detail, and heart 
-        into every project.
-
-
+        I am a creative professional with over 3 years of experience in graphic design, 
+        brand identity, UI/UX design, and video editing. I specializes in turning ideas
+        into impactful visuals and building authentic brand identities that resonate.
+        I am known for my strategic thinking, customer-first approach, and eye for detail, 
+        I bring clarity, creativity, and purpose to every project.
       </p>
     </div>
   )
